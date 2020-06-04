c18af5bb198891130eeff6504e7eef173d9d559e
Eduardo Cerqueira <bold022@einstein.br>
2020-06-04 00:10:09 -0300

    add tracking

---
+++ Word Tracking/Word Tracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word Tracking
+//
+//  Created by Eduardo Cerqueira on 03/06/20.
+//  Copyright © 2020 Eduardo Cerqueira. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
