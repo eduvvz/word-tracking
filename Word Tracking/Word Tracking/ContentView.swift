dc41e45244d2738d8baadf6e070eca0a714c12ef
Eduardo Cerqueira <bold022@einstein.br>
2020-06-03 23:00:44 -0300

    first commit

---
+++ Word Tracking/Word Tracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Word Tracking
+//
+//  Created by Eduardo Cerqueira on 03/06/20.
+//  Copyright © 2020 Eduardo Cerqueira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Coé!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
