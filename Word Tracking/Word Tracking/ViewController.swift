69bf41df2405c70c0c824e94534b514fb6664342
Eduardo Cerqueira <bold022@einstein.br>
2020-06-05 23:53:12 -0300

    add light to scene and random position for box

--- Word Tracking/Word Tracking/ViewController.swift
+++ Word Tracking/Word Tracking/ViewController.swift
@@ -17,14 +17,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true;
         // Do any additional setup after loading the view.
     }
     
     @IBAction func add(_ sender: Any) {
         let node = SCNNode()
         node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0,0,0)
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
+        node.position = SCNVector3(x,y,z)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
 
@@ -38,6 +43,10 @@ class ViewController: UIViewController {
             node.removeFromParentNode()
         }
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
 }
 
